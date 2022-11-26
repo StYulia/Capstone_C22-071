3ddba2d2f54750524fc9c02d0742c3ae9d9b51d4
Adrian Juansyah Hasan <79002078+T-skyCode@users.noreply.github.com>
2022-11-26 17:37:42 +0700

    Update Demo Deployment

---
+++ DemoDeployment/script.js
@@ -0,0 +1,86 @@
+"use strict";
+
+const elemImage = document.getElementById("elemImage");
+const elemPrediksi = document.getElementById("elemPrediksi");
+const elemDetail = document.getElementById("elemDetail");
+
+const renderImage = (input) => {
+  // Menampilkan gambar yang diinputkan
+  elemImage.src = window.URL.createObjectURL(input.files[0]);
+}
+
+/** MODEL HANDLER **/
+let model = null;
+
+(async () => {
+  // Proses memuat model
+  console.log("[Start] Memuat Model");
+  model = await tf.loadLayersModel("tfjs-model/model.json");
+  console.log("[Finish] Model Telah Dimuat");
+})();
+
+const label = [ // Harus urut abjad
+  "Organic",
+  "Recyclable"
+]
+
+const predictImage = async () => {
+  // 1) Cek dulu apakah model sudah dimuat atau belum
+  if (model == null) {
+    alert("Harap Tunggu, Model Belum Selesai Dimuat");
+    return;
+  }
+  // Cek juga sebelum prediksi, gambar sudah diinputkan
+  if (!elemImage.getAttribute('src')) {
+    alert("Harap inputkan gambar terlebih dulu yaa");
+    return;
+  }
+  // 2) Mengambil data gambar pada tag image yang telah dirender
+  let tensor = tf.browser.fromPixels(elemImage);
+  // 3) Menyesuaikan ukuran tensor dengan ukuran input pada model
+  tensor = tensor.resizeNearestNeighbor([224, 224]);
+  // 4) Lakukan normalisasi ukuran pixel dari (0, 255) -> (0, 1)
+  tensor = tensor.div(tf.scalar(255));
+  // 5) Sesuaikan dimensi tensor dengan dimensi input pada model
+  // Contoh: dari [x] menjadi [[x]], x = tensor
+  tensor = tensor.expandDims();
+  // 6) Melakukan proses prediksi
+  let dataPrediksi = await model.predict(tensor).data();
+  // [0.999, 0.5, 0.2]
+  // 7) Mengolah hasil prediksi untuk ditampilkan
+  // bentuk hasil yang diinginkan
+  // [
+  //   {
+  //     class: "Paper (Kertas)",
+  //     confidence: 0.95
+  //   },
+  //   {
+  //     class: "Rock (Batu)",
+  //     confidence: 0.72
+  //   },
+  //   {
+  //     class: "Scissor (Gunting)",
+  //     confidence: 0.4
+  //   }
+  // ]
+  dataPrediksi = Array.from(dataPrediksi).map((value, idx) => {
+    return {
+      class: label[idx],
+      confidence: value
+    }
+  });
+  
+  // Mengurutkan hasil prediksi tinggi->rendah dari nilai confidence
+  dataPrediksi.sort((x, y) => y.confidence - x.confidence);
+  console.log(dataPrediksi);
+
+  // 8) Menampilkan hasil prediksi
+  // Menampilkan top-1 (nilai confidence tertinggi)
+  elemPrediksi.innerHTML = dataPrediksi[0].class;
+  // Menampilkan detail (opsional)
+  let htmlData = "";
+  dataPrediksi.forEach((data) => {
+    htmlData += `<li>${data.class} &rarr; ${data.confidence}</li>`;
+  });
+  elemDetail.innerHTML = htmlData;
+}
