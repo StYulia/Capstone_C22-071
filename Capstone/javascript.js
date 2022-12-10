960d9ca47948f7bbb31db1fb4fea22ac29698d09
Adrian Juansyah Hasan <79002078+ClinoCode@users.noreply.github.com>
2022-12-10 12:50:36 +0700

    Finalisasi Project

--- Capstone/javascript.js
+++ Capstone/javascript.js
@@ -64,7 +64,9 @@ const takephoto = document.getElementById('take-picture');
 const overlay = document.querySelector("#overlay");
 
 let WebcamControl = document.getElementById("webcam-control")
+const takepoto = document.getElementById('taketake')
 
+const kontenPrediksi = document.getElementById('konten');
 
 function getPicture() {
 
@@ -82,10 +84,14 @@ function getPicture() {
     .catch(err => {
         console.log(err);
     });
+    takepoto.style.visibility = 'visible';
     WebcamControl.innerHTML = "Tutup kamera";
+    WebcamControl.style.backgroundColor = 'red'
 
   }else{
     WebcamControl.value="On";
+    WebcamControl.style.backgroundColor = '#6EAB3B';
+    takepoto.style.visibility = 'hidden';
     webcam.stop();
     console.log("webcam stopped");
     WebcamControl.innerHTML = "Buka kamera";
@@ -94,6 +100,9 @@ function getPicture() {
   }
 }
 
+overlay.addEventListener('scroll', () => {
+  const scrolled = overlay.scrollY;
+})
 
 function snapPhoto() {
 
@@ -117,9 +126,9 @@ function closeBtn() {
   overlay.style.display = "none";
   canvass.classList.add("d-none");
 
-  
-
   WebcamControl.value="On";
+  WebcamControl.style.backgroundColor = '#6EAB3B';
+  takepoto.style.visibility = 'hidden';
   WebcamControl.innerHTML = "Buka kamera";
 }
 
@@ -164,8 +173,8 @@ let model = null;
 })();
 
 const label = [ // Harus urut abjad
-  "Sampah ini berjenis Organic",
-  "Sampah ini berjenis Non-Organic"
+  "Organik",
+  "Anorganik"
 ]
 
 const predictImage = async (gambar) => {
@@ -194,11 +203,11 @@ const predictImage = async (gambar) => {
   // bentuk hasil yang diinginkan
   // [
   //   {
-  //     class: "Non-Organic",
+  //     class: "Anorganik",
   //     confidence: 0.95
   //   },
   //   {
-  //     class: "Organic",
+  //     class: "Organik",
   //     confidence: 0.72
   //   }
   // ]
@@ -215,12 +224,50 @@ const predictImage = async (gambar) => {
 
   // 8) Menampilkan hasil prediksi
   // Menampilkan top-1 (nilai confidence tertinggi)
-  elemPrediksi.innerHTML = dataPrediksi[0].class;
+  let prediksiPertama = dataPrediksi[0].class;
+  elemPrediksi.innerHTML = 'Sampah ini berjenis ' + prediksiPertama;
   // Menampilkan detail (opsional)
-  let htmlData = "";
-  dataPrediksi.forEach((data) => {
-    htmlData += `<li>${data.class} &rarr; ${data.confidence}</li>`;
-  });
-  elemDetail.innerHTML = htmlData;
-}
+  if (prediksiPertama == 'Organik') {
+    kontenPrediksi.innerHTML =`
+    <p>
+    Hasil prediksi menunjukkan sampah ini termasuk dalam jenis sampah Organik. Sampah organik merupakan sampah hasil sisa dari organisme berasal dari makhluk hidup, baik manusia, tumbuhan, maupun hewan. 
+    <br><br>
+    Sampah Organik sendiri terbagi menjadi dua jenis yaitu sampah Organik kering dan basah. Sampah organik kering adalah sampah Organik yang kandungan airnya sedikit seperti ranting pohon, kayu, daun-daunan kering, dan sebagainya. 
+    <br><br>
+    Sedangkan sampah organik basah adalah sampah yang sebagian besar mengandung air yang biasanya menjadi alasan kalau sampah ini menimbulkan bau tidak sedap contohnya buah yang busuk, sisa sayur-sayuran, kotoran hewan, kulit pisang, dan sejenisnya.
+    <br><br>
+    Dari data Kementrian Lingkungan Hidup dan Kehutanan (KLHK) sampah organi menjadi salah satu jenis sampah paling banyak dihasilkan di Indonesia. Bahkan jumlahnya mencapai 28,3% dari jumlah timbulan sampah 21,63 juta ton di awal tahun 2022. 
+    <br><br>
+    Pemanfaatan sampah organik ini biasanya sering dimanfaatkan menjadi pupuk kompos atau pupuk organik dan tambahan pakan hewan, loh. Jadi bijak dalam mengelola sampahmu dan dapatkan manfaat dari membuang sampah yang sesuai ya!
 
+
+  
+    </p>`;
+  } else {
+    kontenPrediksi.innerHTML =`
+    <p>
+    Hasil prediksi menunjukkan sampah tersebut termasuk dalam jenis sampah Anorganik. Sampah Anorganik merupakan jenis sampah yang berasal dari organisme tidak hidup dan tidak dapat terurai secara langsung oleh bakteri dan membutuhkan unsur kimia dalam proses penguraian. 
+    <br><br>
+    Jenis sampah Anorganik terdiri dari dua bentuk yaitu, sampah Anorganik lunak dan Anorganik keras. Anorganik lunakan merupakan jenis sampah non alami yang mudah dibentuk seperti sampah plastik, bungkus kemasan, bahan tekstil, dan sebagainya. Sedangkan sampah Anorganik keras limbah yang sulit untuk diolah Kembali antara lain seperti sampah kaleng, kaca, bahan metal, atau material pecah belah.
+    <br><br>
+    Melansir dari data Kementrian Lingkungan Hidup dan Kehutan (KHLK), sampah Anorganik berjumlah 35,62%, hasil gabungan dari sampah plastik 15,73%, logam, 6,68%, kain 6,57%, dan kaca sebanyak 6,46% dan dari jumlah sampah-sampah itu, masih ada 33,49% sampah yang belum dikelola dengan baik dengan sistem pengelolaan sampah di Indonesia yang masih kurang efektif. 
+    <br><br>
+    Selain menyebabkan dampak yang buruk untuk lingkungan mahkluk hidup, Pemanfaatan sampah Anorganik bisa dimanfaatkan untuk menjadi kerajinan tangan atau daur ulang lainnya. Hasil daur ulang dari sampah Anorganik ini juga memiliki nilai jual loh, jadi jangan lupa bijak membuang sampah dan manfaatkan sampah Anorganik kalian menjadi daur ulang yang bermanfaat ya!
+    </p>`;
+  }
+}
+let nodeList = document.querySelector(".buttons");
+
+$('.buttons').click(function(){
+
+  $(this).addClass('button-active').siblings().removeClass('button-active');
+
+  let filter = $(this).attr('data-filter');
+  if(filter == 'all'){
+      $('.menu .image').show(400);
+  }else{
+      $('.menu .image').not('.'+filter).hide(200);
+      $('.menu .image').filter('.'+filter).show(400);
+  }
+
+});
